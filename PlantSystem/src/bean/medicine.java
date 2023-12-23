36c5bc45bbf28f92bc5e1da8c9809152fe58d230
Kyenlyn <147908856+Kyenlyn@users.noreply.github.com>
2023-12-23 22:39:16 +0800

    病虫害、防治方法、药剂 表

---
+++ PlantSystem/src/bean/medicine.java
@@ -0,0 +1,40 @@
+package Disease;
+//药剂表
+public class medicine {
+	String medicineID;//药剂编号
+	String medicineName;//药剂名称
+	String medicineDosage;//药剂用量
+	String medicineDuration;//作用期限
+	
+    public medicine(String medicineID, String medicineName, String medicineDosage, String medicineDuration) {  
+        this.medicineID = medicineID;
+        this.medicineName = medicineName;  
+        this.medicineDosage = medicineDosage;
+        this.medicineDuration = medicineDuration;
+    }  
+	
+	public String get_medicineID() {
+		return this.medicineID;
+	}
+	public void set_medicineID(String medicineID) {
+		this.medicineID = medicineID;
+	}
+	public String get_medicineName() {
+		return this.medicineName;
+	}
+	public void set_medicineName(String medicineName) {
+		this.medicineName = medicineName;
+	}
+	public String get_medicineDosage() {
+		return this.medicineDosage;
+	}
+	public void set_medicineDosage(String medicineDosage) {
+		this.medicineDosage = medicineDosage;
+	}
+	public String get_medicineDuration() {
+		return this.medicineDuration;
+	}
+	public void set_medicineDuration(String medicineDuration) {
+		this.medicineDuration = medicineDuration;
+	}
+}
