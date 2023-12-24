714ad8b0e89afc7564b7ec4084735c231ed40738
Kyenlyn <147908856+Kyenlyn@users.noreply.github.com>
2023-12-24 22:22:20 +0800

    Update病虫害表

--- PlantSystem/src/bean/disease.java
+++ PlantSystem/src/bean/disease.java
@@ -1,9 +1,9 @@
 package Disease;
 //病虫害表
 public class disease {
-	String diseaseID;//病虫害编号
-	String diseaseName;//病虫害名称
-	String treatmentID;//防治方法编号
+	private String diseaseID;//病虫害编号
+	private String diseaseName;//病虫害名称
+	private String treatmentID;//防治方法编号
 	
     public disease(String diseaseID, String diseaseName, String treatmentID) {  
         this.diseaseID = diseaseID;
