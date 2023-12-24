e4bdfc3b89dfa38e5bcfca8bc3fe82e97cb8b626
zoey <zyue6160@gmail.com>
2023-12-24 12:23:23 +0800

    植物基本信息表

--- PlantSystem/src/bean/PlantInfo.java
+++ PlantSystem/src/bean/PlantInfo.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class PlantInfo {
 	    private String plantId;
 	    private String plantName;
-	    private String diseaseID;
+	    private String diseaseID;//病编号
 	    private String features;
 	    private String cultivationTechniques;
 	    private String value;
