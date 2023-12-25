29ebeae0931748d605079caaa14ced762f62716c
YTJY <107770509+YiTiaoJingYu@users.noreply.github.com>
2023-12-25 12:05:21 +0800

    Update Sort.java
    增加了SortId

--- PlantSystem/src/bean/Sort.java
+++ PlantSystem/src/bean/Sort.java
@@ -1,54 +1,76 @@
 package bean;
 
-// 定义Sort实体类
 public class Sort {
-    // 定义私有属性，对应数据库表的字段
-    private String speciesId; // 物种编号，外键
-    private String countyId; // 县级编号，外键
-    private String plantId; // 所属植物，外键
-    private String growEnv; // 生长环境
-    private String alias; // 别名
 
-    // 定义构造方法，用于创建对象
-    public Sort() {} // 空参构造方法
-    public Sort(String speciesId, String countyId, String plantId, String growEnv, String alias) { // 全参构造方法
-        this.speciesId = speciesId;
+    private String sortId;
+    private String genusId;
+    private String countyId;
+    private String plantId;
+    private String growEnv;
+    private String alias;
+
+    public Sort() {
+        // 默认构造函数
+    }
+
+    public Sort(String sortId, String genusId, String countyId, String plantId, String growEnv, String alias) {
+        this.sortId = sortId;
+        this.genusId = genusId;
         this.countyId = countyId;
         this.plantId = plantId;
         this.growEnv = growEnv;
         this.alias = alias;
     }
 
-    // 定义setter和getter方法，用于属性的赋值和取值
-    public String getSpeciesId() {
-        return speciesId;
+    // Getter and Setter methods
+
+    public String getSortId() {
+        return sortId;
     }
-    public void setSpeciesId(String speciesId) {
-        this.speciesId = speciesId;
+
+    public void setSortId(String sortId) {
+        this.sortId = sortId;
     }
+
+    public String getGenusId() {
+        return genusId;
+    }
+
+    public void setGenusId(String genusId) {
+        this.genusId = genusId;
+    }
+
     public String getCountyId() {
         return countyId;
     }
+
     public void setCountyId(String countyId) {
         this.countyId = countyId;
     }
+
     public String getPlantId() {
         return plantId;
     }
+
     public void setPlantId(String plantId) {
         this.plantId = plantId;
     }
+
     public String getGrowEnv() {
         return growEnv;
     }
+
     public void setGrowEnv(String growEnv) {
         this.growEnv = growEnv;
     }
+
     public String getAlias() {
         return alias;
     }
+
     public void setAlias(String alias) {
         this.alias = alias;
     }
-}
 
+    // You can add other methods or overrides as needed
+}
