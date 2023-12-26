41d1d3f4b865cce5a9e9cf3ca609fb570fab06cd
Zoeeyue <152131046+Zoeeyue@users.noreply.github.com>
2023-12-27 00:36:15 +0800

    业务线一接入登录逻辑框架v1

--- PlantSystem/src/service/login_service.java
+++ PlantSystem/src/service/login_service.java
@@ -1,15 +1,20 @@
 package service;
-
+import java.util.Map;
 import java.util.Scanner;
 import DAO.admin_infoDAO;
 import DAO.Impl.admin_infoDAOImpl;
+import DAO.Impl.image_infoDAOImpl;
+import bean.PlantInfo;
 import bean.adminInfo;
 import bean.staff;
-import DAO.staffDAO;
+//import DAO.staffDAO;
 import DAO.Impl.plant_infoDAOImpl;
-import DAO.Impl.staffDAOImpl;
+//import DAO.Impl.staffDAOImpl;
 
 public class login_service {
+	
+	
+	
 	//选择角色
 	public void roleChoose() {
 		Scanner scanner = new Scanner(System.in);
@@ -27,7 +32,7 @@ public class login_service {
 //					getupkeepInfoFromUser(scanner);
 					break;
 				case 4:
-					getstaffInfoFromUser(scanner);
+//					getstaffInfoFromUser(scanner);
 					break;
 				default:
 					System.out.println("序号无效，请重新输入！");
@@ -76,16 +81,16 @@ public class login_service {
 		}
 	}
 	
-//	private static void getupkeepInfoFromUser() {
-//	Scanner scanner = new Scanner(System.in);
-//		System.out.println("您选择以【养护人员】身份登录，请输入您的账号：");
-//		String Id = scanner.nextLine();
-//        System.out.println("请输入您的密码: ");
-//        String password = scanner.nextLine();
-//        UpkeepStaff staff = new UpkeepStaff(Id,"",password);
-	/////
-//	}
-	
+	private static void getupkeepInfoFromUser() {
+	Scanner scanner = new Scanner(System.in);
+		System.out.println("您选择以【养护人员】身份登录，请输入您的账号：");
+		String Id = scanner.nextLine();
+        System.out.println("请输入您的密码: ");
+        String password = scanner.nextLine();
+        UpkeepStaff staff = new UpkeepStaff(Id,"",password);
+	///
+	}
+//	
 	private static void getstaffInfoFromUser(Scanner scanner) {
 		System.out.println("您选择以【监测人员】身份登录，请输入您的账号：");
 		String Id = scanner.nextLine();
@@ -118,52 +123,123 @@ public class login_service {
 	//角色功能菜单
 	private static void displayPlantInfoMenu(Scanner scanner) {
         while (true) {
+        	//实例化业务
+        	plantinfo_service plantinfo_service =new plantinfo_service();
+        	//实例化DAO接口
+        	plant_infoDAOImpl plantDAO=new plant_infoDAOImpl();
+        	image_infoDAOImpl imageDAO =new image_infoDAOImpl();
             // 显示植物信息管理菜单
             System.out.println("植物基本信息管理菜单:");
-            System.out.println("1. 查询植物信息");
-            System.out.println("2. 添加植物信息");
-            System.out.println("3. 更新植物信息");
-            System.out.println("4. 删除植物信息");
-            System.out.println("5. 统计平台中每科植物的数量");
-            System.out.println("6. 根据任意属性或属性组合查询所需的园林植物");
-            System.out.println("7. 退出");
+	        System.out.println("1. 查询所有植物信息");
+	        System.out.println("2. 添加植物信息");
+	        System.out.println("3. 更新植物信息");
+	        System.out.println("4. 删除植物信息");
+	        System.out.println("5. 统计平台中每科植物的数量");
+	        System.out.println("6. 根据任意属性或属性组合查询所需的园林植物信息");
+	        System.out.println("7. 退出该基本管理信息菜单");
             // 获取用户输入
             int choice = scanner.nextInt();
             scanner.nextLine();
             // 使用 switch-case 处理不同选择
             switch (choice) {
                 case 1:
-                    System.out.println("查询植物信息");
+                    System.out.println("查询所有植物信息");
                     // 调用查询植物信息的逻辑
-                    plant_infoDAOImpl plantDao=new plant_infoDAOImpl();
 				try {
-					plantDao.queryAllPf();
+					plantinfo_service.displayQuery(plantDAO.queryAllPf());
 				} catch (Exception e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
                     break;
                 case 2:
                     System.out.println("添加植物信息");
                     // 调用添加植物信息的逻辑
+                    plantinfo_service.displayInsertPlant(plantDAO,imageDAO);
                     break;
                 case 3:
                     System.out.println("更新植物信息");
                     // 调用更新植物信息的逻辑
+                    
+    				try {
+					System.out.println("有以下植物可以更新，请选择");
+					plantinfo_service.displayQuery(plantDAO.queryAllPf());
+                    System.out.println("请输入需要更新的植物编号：");
+        	        String plantId = scanner.nextLine();
+					if(!plantDAO.isPlantIdExists(plantId))
+				    {
+				    System.out.println("该植物不存在！");
+                    break;
+                    }else {
+                    System.out.println("以下是你选择更新的植物信息 ：");
+                    plantinfo_service.displayQuery(plantDAO.queryFromViewById(plantId));
+                    PlantInfo plantinfo=plantDAO.getPlantInfoById(plantId);
+                    plantinfo_service.updateSelect(scanner, plantDAO, plantinfo);
+                    
+                    }
+				} catch (Exception e) {
+					
+					e.printStackTrace();
+				}
                     break;
                 case 4:
                     System.out.println("删除植物信息");
                     // 调用删除植物信息的逻辑
+
+					 System.out.println("有以下植物可以删除，请选择");
+				try {
+					plantinfo_service.displayQuery(plantDAO.queryAllPf());
+				    System.out.println("请输入需要删除的植物编号：");
+        	        String deleteId = scanner.nextLine();
+        	        if(!plantDAO.isPlantIdExists(deleteId))
+        	        {
+        	        	System.out.println("该植物不存在！");
+        	        	break;
+               }else {
+            	   plantinfo_service.displayDeletePlant(plantDAO, imageDAO, deleteId);
+               }
+					
+					
+				} catch (Exception e) {
+					
+					e.printStackTrace();
+				}
+	                
+
                     break;
-                case 5:
-                    System.out.println("删除植物信息");
-                    // 调用删除植物信息的逻辑
-                    break;
-                case 6:
-                    System.out.println("退出");
-                    // 退出菜单循环
-//                    scanner.close();
-//                    System.exit(0);
+                  case 5:
+                  System.out.println("统计平台中每科植物的数量");
+                  // 调用统计植物信息的逻辑
+				try {
+					plantDAO.countPlantsByScientificName();
+				} catch (Exception e) {
+				
+					e.printStackTrace();
+				}
+                  break;
+              case 6:
+                  System.out.println("根据任意属性或属性组合查询所需的园林植物信息");
+                  // 调用根据任意属性或属性组合查询所需的园林植物信息的逻辑
+                  // 提示用户输入查询条件
+                  System.out.println("请输入查询条件（属性及值），多个条件之间用逗号分隔，例如：植物种名=野迎春,植物编号=0001");
+                  String userInput = scanner.nextLine();
+
+                  // 解析用户输入，构建属性及其值的映射
+                  Map<String, Object> properties =  plantinfo_service.parseUserInput(userInput);
+
+                  // 调用查询逻辑
+                  try {
+                	  
+                	 plantinfo_service.displayQuery(plantDAO.queryFromViewByProperties(properties));
+                     
+                  } catch (Exception e) {
+                      e.printStackTrace();
+                  }
+                  break;
+              case 7:
+                  System.out.println("退出该基本管理信息菜单");
+                  // 退出菜单循环
+                  scanner.close();
+                  System.exit(0);
                 default:
                     System.out.println("无效选择，请重新输入");
                     break;
@@ -291,4 +367,5 @@ public class login_service {
 		login_service l = new login_service();
 		l.roleChoose();
 	 }
+
 }
