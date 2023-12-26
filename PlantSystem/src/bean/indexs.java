cafe72a23493d61a762cecd4ca2081e00a5a1a31
Kyenlyn <147908856+Kyenlyn@users.noreply.github.com>
2023-12-26 10:22:43 +0800

    Update指标

--- PlantSystem/src/bean/indexs.java
+++ PlantSystem/src/bean/indexs.java
@@ -1,14 +1,16 @@
-package monitor;
+package bean;
 //监测指标
 public class indexs {
 	private String indexID;//指标编号
 	private String indexName;//指标名称
 	private String indexValue;//指标值
+	private String monitorID;//监测编号
 	
-    public indexs(String indexID, String indexName, String indexValue) {  
+    public indexs(String indexID, String indexName, String indexValue, String monitorID) {  
         this.indexID = indexID;
         this.indexName = indexName;
         this.indexValue = indexValue;
+        this.monitorID = monitorID;
     }  
 	
     public String get_indexID() {  
@@ -33,5 +35,13 @@ public class indexs {
   
     public void set_indexValue(String indexValue) {  
         this.indexValue = indexValue;  
+    }  
+	
+    public String get_monitorID() {  
+        return this.monitorID;  
+    }  
+  
+    public void set_monitorID(String monitorID) {  
+        this.monitorID = monitorID;  
     }  
 }
