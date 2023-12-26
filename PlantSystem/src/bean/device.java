c4035826b68750f50c66a885305cad3ee060add8
Kyenlyn <147908856+Kyenlyn@users.noreply.github.com>
2023-12-26 10:25:29 +0800

    Update监测设备

--- PlantSystem/src/bean/device.java
+++ PlantSystem/src/bean/device.java
@@ -1,14 +1,12 @@
-package monitor;
+package bean;
 //监测设备表
 public class device {
 	private String deviceID;//设备编号
 	private String deviceName;//设备名称
-	private String indexID;//指标编号
 	
-    public device(String deviceID, String deviceName, String indexID) {  
+    public device(String deviceID, String deviceName) {  
         this.deviceID = deviceID;
         this.deviceName = deviceName;
-        this.indexID = indexID;
     }  
 	
     public String get_deviceID() {  
@@ -25,13 +23,5 @@ public class device {
   
     public void set_deviceName(String deviceName) {  
         this.deviceName = deviceName;  
-    }  
-  
-    public String get_indexID() {  
-        return this.indexID;  
-    }  
-  
-    public void set_indexID(String indexID) {  
-        this.indexID = indexID;  
     }  
 }
