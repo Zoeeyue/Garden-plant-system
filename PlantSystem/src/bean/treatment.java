3bf4d644566b0b79e251400bbf93669326c387e0
Kyenlyn <147908856+Kyenlyn@users.noreply.github.com>
2023-12-26 10:14:57 +0800

    Update防治方法

--- PlantSystem/src/bean/treatment.java
+++ PlantSystem/src/bean/treatment.java
@@ -4,13 +4,13 @@ public class treatment {
 	private String treatmentID;//方法编号
 	private String treatmentName;//方法名称
 	private String treatmentCont;//方法内容
-	private String medicineID;//药剂编号
+	private String diseaseID;//病虫害编号
 	
-    public treatment(String treatmentID, String treatmentName, String treatmentCont, String medicineID) {  
+    public treatment(String treatmentID, String treatmentName, String treatmentCont, String diseaseID) {  
         this.treatmentID = treatmentID;  
         this.treatmentName = treatmentName;
         this.treatmentName = treatmentCont;
-        this.medicineID = medicineID;
+        this.diseaseID = diseaseID;
     }  
 	
 	public String get_treatmentID() {  
@@ -37,11 +37,11 @@ public class treatment {
         this.treatmentCont = treatmentCont;  
     }  
   
-    public String get_medicineID() {  
-        return this.medicineID;  
+    public String get_diseaseID() {  
+        return this.diseaseID;  
     }  
   
-    public void set_medicineID(String medicineID) {  
-        this.medicineID = medicineID;  
+    public void set_diseaseID(String diseaseID) {  
+        this.diseaseID = diseaseID;  
     }  
 }
